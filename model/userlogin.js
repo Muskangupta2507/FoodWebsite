edcfab4b9b3a220d844eb5e0ead59a34d64fbaaf
Muskangupta2507 <100120446+Muskangupta2507@users.noreply.github.com>
2023-11-22 14:28:03 +0530

    xyz

---
+++ model/userlogin.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+	name:{
+		type: String,
+		// required: true,
+	},
+	email:{
+		type: String,
+		// required: true,
+	},
+	password:{
+		type: String,
+		// required: true,
+	},
+	createdAt: {
+		type: Number,
+		default: new Date().getTime(),
+	},
+	isActive: {
+		type: Boolean,
+		// required: true,
+	}
+})
+
+module.exports = mongoose.model('users', postSchema);
